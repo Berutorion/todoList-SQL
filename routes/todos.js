6f1af869efebc2b2dbfa6afc1672bc1da62cb4ae
Eagle1630 <eagle163013@gmail.com>
2022-09-10 23:49:43 +0800

    feat: complete  todo's basic CRUD

--- routes/todos.js
+++ routes/todos.js
@@ -1,13 +1,30 @@
 const router = require("express").Router();
 
 const db = require("../models");
-const User = db.User;
 const Todo = db.Todo;
 
+//新增資料
+router.get("/new", (req, res) => {
+  res.render("new");
+});
+
+router.post("/", async (req, res) => {
+  const userId = req.user.id;
+  const { name } = req.body;
+
+  try {
+    await Todo.create({ name, UserId: userId });
+    req.flash("success_msg", "新增成功");
+    res.redirect("/todos");
+  } catch (error) {
+    console.log(error);
+  }
+});
+//查詢全部
 router.get("/", (req, res) => {
-    const userId = req.user.id;
+  const userId = req.user.id;
   return Todo.findAll({
-    where:{userId},
+    where: { userId },
     raw: true,
     nest: true,
   })
@@ -18,14 +35,59 @@ router.get("/", (req, res) => {
       console.log(error);
     });
 });
-
-router.get("/:id", (req, res) => {
+//查詢一筆
+router.get("/:id", async (req, res) => {
   const id = req.params.id;
-  return Todo.findByPk(id).then((todo) => {
-    res.render("detail", { todo: todo.toJSON() }).catch((error) => {
-      console.log(error);
-    });
-  });
+  const userId = req.user.id;
+  try {
+    const todo = await Todo.findOne({ where: { id, userId } });
+    res.render("detail", { todo: todo.toJSON() });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//修改資料
+
+router.get("/:id/edit", async (req, res) => {
+  const id = req.params.id;
+  const userId = req.user.id;
+  try {
+    const todo = await Todo.findOne({ where: { id, userId } });
+    res.render("edit", { todo: todo.toJSON() });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const userId = req.user.id;
+  const { name, isDone } = req.body;
+  try {
+    await Todo.update(
+      { name, isDone: isDone === "on" ? 1 : 0 },
+      { where: { id, userId } }
+    );
+    req.flash("success_msg", "修改成功");
+    res.redirect("/todos");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//刪除資料
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  const userId = req.user.id;
+
+  try {
+    Todo.destroy({ where: { id, userId } });
+    req.flash("success_msg", "刪除成功");
+    res.redirect("/todos");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -24,15 +24,32 @@ router.get("/register", (req, res) => {
 
 router.post("/register", async (req, res) => {
   const { name, email, password, confirmPassword } = req.body;
-
+  let errors = [];
   try {
-    const user = await User.findOne({ where: { email } });
-    if (user) {
-      console.log("User already exists");
-      return res.render("register", { name, email, password, confirmPassword });
+    //確認信箱是否重複
+    if (await User.findOne({ where: { email } }))
+      errors.push({ message: "這個信箱已經註冊!!" });
+    //確認欄位是否為空值
+    if (!name || !email || !password || !confirmPassword)
+      errors.push({ message: "所有欄位為必填!!" });
+    //確認兩次輸入的密碼是否為相同
+    if (confirmPassword != password)
+      errors.push({ message: "密碼與確認密碼不相符!!" });
+    if (errors.length) {
+      req.flash("warning_msg", errors[0].message);
+      return res.render("register", {
+        name,
+        email,
+        password,
+        confirmPassword,
+        warning_msg: req.flash("warning_msg"),
+      });
     }
+
+    //將註冊資料寫入資料庫
     const hash = await bcrypt.hash(password, await bcrypt.genSalt(10));
     await User.create({ name, email, password: hash });
+    req.flash("success_msg", "註冊成功，請重新登入");
     res.redirect("/users/login");
   } catch (error) {
     console.log(error);
